ee2c2ca8f49a8276966b258b238b7545ccb47e57
Hasan Sarkar <hasansarkar@Hasans-MacBook-Air.local>
2025-06-28 05:53:55 +0600

    Convert VIDEASY from source to embed provider - Remove source version and update registration

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -3,7 +3,7 @@ import { doodScraper } from '@/providers/embeds/dood';
 import { mixdropScraper } from '@/providers/embeds/mixdrop';
 import { turbovidScraper } from '@/providers/embeds/turbovid';
 import { upcloudScraper } from '@/providers/embeds/upcloud';
-import { videasyScraper as videasyEmbedScraper } from '@/providers/embeds/videasy';
+import { videasyScraper } from '@/providers/embeds/videasy';
 import { autoembedScraper } from '@/providers/sources/autoembed';
 import { catflixScraper } from '@/providers/sources/catflix';
 import { ee3Scraper } from '@/providers/sources/ee3';
@@ -12,7 +12,6 @@ import { insertunitScraper } from '@/providers/sources/insertunit';
 import { mp4hydraScraper } from '@/providers/sources/mp4hydra';
 import { tugaflixScraper } from '@/providers/sources/tugaflix';
 import { vidsrcsuScraper } from '@/providers/sources/vidsrcsu';
-import { videasyScraper as videasySourceScraper } from '@/providers/sources/videasy';
 
 import {
   autoembedBengaliScraper,
@@ -99,7 +98,6 @@ export function gatherAllSources(): Array<Sourcerer> {
     wecimaScraper,
     animeflvScraper,
     cinemaosScraper,
-    videasySourceScraper,
   ];
 }
 
@@ -144,7 +142,7 @@ export function gatherAllEmbeds(): Array<Embed> {
     streamwishSpanishScraper,
     streamwishEnglishScraper,
     streamtapeLatinoScraper,
-    videasyEmbedScraper,
+    videasyScraper,
     ...cinemaosEmbeds,
     // ...cinemaosHexaEmbeds,
   ];
--- src/providers/embeds/videasy.ts
+++ src/providers/embeds/videasy.ts
@@ -4,7 +4,7 @@ import { EmbedScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
 
 export const videasyScraper = makeEmbed({
-  id: 'videasy-embed',
+  id: 'videasy',
   name: 'VIDEASY',
   rank: 200,
   disabled: false,
